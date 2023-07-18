aa03be0d8cb046999ff9393f9dfcc268cc4ff891
Alex Dillhoff <ajdillhoff@gmail.com>
2023-07-18 09:52:30 -0500

    Added small clarification.

--- arrays/2darray_contiguous.c
+++ arrays/2darray_contiguous.c
@@ -9,6 +9,10 @@ int main() {
         }
     }
 
+    // Note that the address of the first element is the same as the address of the array
+    printf("a = %p\n", a);
+    printf("a[0] = %p\n", a[0]);
+
     // Print the addresses of the first dimension
     printf("Only the first dimension...\n");
     for (int i = 0; i < 4; i++) {
